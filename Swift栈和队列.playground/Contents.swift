9212253a7c8afeb75e9dad3b05167d40ece4715a
Pei丶Code <aishiklpz@163.com>
2019-04-03 18:44:29 +0800

    [lpz]Queue&Tree

--- Swift栈和队列.playground/Contents.swift
+++ Swift栈和队列.playground/Contents.swift
@@ -74,3 +74,68 @@ class Queue {
         return left.last
     }
 }
+
+//Definition for a binary tree node.
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init(_ val: Int) {
+          self.val = val
+          self.left = nil
+          self.right = nil
+    }
+}
+
+class Solution {
+    func levelOrder(_ root: TreeNode?) -> [[Int]] {
+        guard let root = root  else {
+            return []
+        }
+        var array: [[Int]] = [[]]
+        var queue: [TreeNode] = []
+//        var left: [Int] = []
+//        var right: [Int] = []
+//        var index = 0
+        queue.append(root)
+        while !queue.isEmpty {
+            let size = queue.count
+            /// 当前level
+            var cur: [Int] = []
+            for _ in 0..<size {
+                let node = queue.removeFirst()
+                cur.append(node.val)
+                if let left = node.left {
+                    queue.append(left)
+                }
+                if let right = node.right {
+                    queue.append(right)
+                }
+            }
+            array.append(cur)
+        }
+        return array
+    }
+
+}
+// 解法二：
+class Solution2 {
+
+    var result = [[Int]]()
+    func levelOrder(_ root: TreeNode?) -> [[Int]] {
+        level(order: 0, root: root)
+        return result
+    }
+    private func level(order: Int, root: TreeNode?) {
+        guard let root = root else {
+            return
+        }
+        if result.count > order {
+            result[order].append(root.val)
+        } else {
+            result.append([root.val])
+        }
+        level(order: order + 1, root: root.left)
+        level(order: order + 1, root: root.right)
+    }
+}
